a3256ada1acfb970d6260aa60cf0bc3bc6626084
romanovalada <loshka130@yandex.ru>
2026-09-22 22:15:10 +0300

    создание разметки для окна регистрации

--- app/src/main/java/com/example/wheretogo/MainActivity.kt
+++ app/src/main/java/com/example/wheretogo/MainActivity.kt
@@ -13,19 +13,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.wheretogo.ui.theme.WhereToGoTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            WhereToGoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            // Вызываем окно логина из LoginActivity
+            val loginActivity = LoginActivity()
+            loginActivity.LoginWindow()
         }
     }
 }
