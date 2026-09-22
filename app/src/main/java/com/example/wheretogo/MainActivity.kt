c5ad353f34f02bf094dd8d11ef63c969fc153029
Souldar <gulyaev.andrey.n@gmail.com>
2026-09-22 23:09:36 +0300

    создание главного экрана

--- app/src/main/java/com/example/wheretogo/MainActivity.kt
+++ app/src/main/java/com/example/wheretogo/MainActivity.kt
@@ -4,13 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.wheretogo.ui.theme.WhereToGoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +37,66 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WhereToGoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Greeting()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        // 1. Фоновое изображение на весь экран
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            contentDescription = "Фон главного экрана",
+            painter = painterResource(id = R.drawable.main_activity),
+            contentScale = ContentScale.Crop
+        )
+        
+        // 2. Колонка для размещения логотипа и текста друг под другом
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.size(400.dp),
+                contentDescription = "Лого WhereToGo",
+                painter = painterResource(id = R.drawable.logo),
+            )
+            Text(
+                text = "Экскурсии, достопримечательности и просто интересные места",
+                textAlign = TextAlign.Center
+            )
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth(0.6f) // Занимает 70% ширины экрана, делая кнопку шире
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 200.dp),
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF315C45), // Цвет фона кнопки (например, синий)
+                    contentColor = Color.White        // Цвет текста кнопки
+                )
+            ) {
+                Text(text = "Войти", fontSize = 20.sp, fontWeight = FontWeight.Bold) // Текст перенесен внутрь тела кнопки
+            }
+            Text (
+                modifier = Modifier.clickable{/*TODO*/},
+                text = "Нет аккаунта? Зарегистрироваться"
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     WhereToGoTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
